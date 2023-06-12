2f1530b5a94e6127d5c3e72b658a90247afe4593
Withor Lucas <withorcello@gmail.com>
2023-06-12 19:26:23 -0400

    refactor: ajusta porta do servidor para evitar conflito

--- src/config/database/mysql-datasource.config.ts
+++ src/config/database/mysql-datasource.config.ts
@@ -1,16 +1,16 @@
-import 'reflect-metadata';
-import { DataSource } from 'typeorm';
+import "reflect-metadata";
+import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root2',
-  password: 'root2',
-  database: 'test_db',
+  type: "mysql",
+  host: "localhost",
+  port: 3360,
+  username: "root",
+  password: "root",
+  database: "dentista_db",
   synchronize: false,
   logging: false,
-  entities: ['src/api/components/**/*.entity{.ts,.js}'],
+  entities: ["src/api/components/**/*.entity{.ts,.js}"],
   migrations: [],
   subscribers: [],
 });
