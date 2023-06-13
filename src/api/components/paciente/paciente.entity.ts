290db56f2490aa8eeeda46314b424a7c9f642637
Withor Lucas <withorcello@gmail.com>
2023-06-12 20:09:33 -0400

    refactor: adiciona validação a entidade de paciente

--- src/api/components/paciente/paciente.entity.ts
+++ src/api/components/paciente/paciente.entity.ts
@@ -1,4 +1,10 @@
+import { IsDate, IsEmail, IsEnum, IsIn, IsInt, IsNotEmpty, IsPhoneNumber, IsString, Length } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+enum SexoPaciente {
+  Maculino = 1,
+  Feminino,
+}
 
 @Entity("paciente")
 export class Paciente {
@@ -6,23 +12,38 @@ export class Paciente {
   id!: number;
 
   @Column()
+  @IsString()
+  @IsNotEmpty()
   nome!: string;
 
   @Column()
+  @IsString()
+  @IsNotEmpty()
+  @Length(11, 11)
   cpf!: string;
 
   @Column("date")
+  @IsDate()
+  @IsNotEmpty()
   data_nascimento!: Date;
 
   @Column()
-  sexo!: "F" | "M";
+  @IsEnum(SexoPaciente)
+  @IsNotEmpty()
+  sexo!: number;
 
   @Column()
+  @IsString()
+  @IsNotEmpty()
   endereco!: string;
 
   @Column()
+  @IsPhoneNumber()
+  @IsNotEmpty()
   celular!: string;
 
   @Column()
+  @IsEmail()
+  @IsNotEmpty()
   email!: string;
 }
